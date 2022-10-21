bd09d09e58065576bf88f41a520b2a431580b07f
Sudhir Kumar <sudhir9122kumar@gmail.com>
2022-10-21 14:13:43 +0530

    21 Oct 2022

---
+++ C program/PowerLoop.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int base;
+    int power;
+    printf("Enter base and power: ");//2 5  = 2^5 = 32
+    scanf("%d%d",&base,&power);
+    int ans=1;
+    for(int i=1;i<=power;i++){
+        ans = ans*base;
+    }
+    printf("%d to the power %d is %d.",base,power,ans);
+    return 0;
+}
