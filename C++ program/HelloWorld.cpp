c796ab689e171abc9b5320f6201b06a44ab24dac
Sudhir Kumar <sudhir9122kumar@gmail.com>
2022-12-17 13:04:51 +0530

    17 Dec 2022

--- C++ program/HelloWorld.cpp
+++ C++ program/HelloWorld.cpp
@@ -1,10 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"* * * * *\n";
-    cout<<"* * * *\n";
-    cout<<"* * *\n";
-    cout<<"* *\n";
-    cout<<"*\n";
+    cout<<"Hello world!";
     return 0;
 }
