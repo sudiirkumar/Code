0151ed1d1f64d1d4471dd2b6490e15906fe5c3bb
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-01-20 14:14:42 +0530

    20 Jan 2023

--- C++ program/FunctionBasic.cpp
+++ C++ program/FunctionBasic.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 /*
 return_type function_name(parameter or arguments){
@@ -8,6 +9,9 @@ return_type function_name(parameter or arguments){
     return;
 }
 */
+int square(int n){
+    return n*n;
+}
 int factorial(int n){
     if(n==0){
         return 1;
@@ -25,10 +29,49 @@ int nCr(int n,int r){
     int ans = factorial(n)/(factorial(r)*factorial(n-r));
     return ans;
 }
+void printNum(int n){
+    for(int i=1;i<=n;i++){
+        if(i==13){
+            return;
+        }
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+bool isEven(int n){
+    if(n%2==0){
+        return true;
+    }
+    return false;
+}
+bool isPrime(int n){
+    if(n==1 || n==0){
+        return false;
+    }
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
 int main(){
-    int n,r;
-    cin>>n>>r;
-    int ans = nCr(n,r);
-    cout<<ans<<endl;
+    // int n,r;
+    // cin>>n>>r;
+    // int ans = nCr(n,r);
+    // cout<<ans<<endl;
+    int n;
+    cin>>n;
+    // cout<<square(n)<<endl;
+    // printNum(n);
+    
+    // if(isEven(n)){
+    //     cout<<"Even";
+    // }
+    // else{
+    //     cout<<"Odd";
+    // }
+    // cout<<(isEven(n)?"Even":"Odd");
+    cout<<(isPrime(n)?"Prime":"Not prime");
     return 0;
 }
