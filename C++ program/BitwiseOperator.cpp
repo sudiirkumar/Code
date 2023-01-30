9f661b353a252fc1cac3cfd566ba416c625397e7
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-01-30 15:06:06 +0530

    30 Jan 2023

--- C++ program/BitwiseOperator.cpp
+++ C++ program/BitwiseOperator.cpp
@@ -4,12 +4,12 @@ int main(){
     int a,b;
     cout<<"Enter 2 numbers: ";
     cin>>a>>b;
-    cout<<"a&b "<<(a&b)<<endl;
-    cout<<"a|b "<<(a|b)<<endl;
-    cout<<"~a "<<(~a)<<endl;
-    cout<<"a>>b "<<(a>>b)<<endl;
-    cout<<"a<<b "<<(a<<b)<<endl;
-    cout<<"a^b "<<(a^b)<<endl;
+    cout<<"a&b "<<(a&b)<<endl;//Bitwise AND
+    cout<<"a|b "<<(a|b)<<endl;//Bitwise OR
+    cout<<"~a "<<(~a)<<endl;//Bitwise NOT
+    cout<<"a>>b "<<(a>>b)<<endl;//Bitwise Right shift
+    cout<<"a<<b "<<(a<<b)<<endl;//Bitwise Left shift
+    cout<<"a^b "<<(a^b)<<endl;//Bitwise XOR
     cout<<"(a^b)^a"<<((a^b)^a)<<endl;
     cout<<"(a^b)^b"<<((a^b)^b)<<endl;
     return 0;
