680df247b492db98d2e904d29d3ca9e462b950a1
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-02-13 09:27:44 +0530

    13 Feb 2023

--- C++ program/PrintSubarrays.cpp
+++ C++ program/PrintSubarrays.cpp
@@ -5,12 +5,10 @@ int main(){
     int n = sizeof(a)/sizeof(int);
     for(int i=0;i<n;i++){
         for(int j=i;j<n;j++){
-            int sum = 0;
             for(int k=i;k<=j;k++){
-                // cout<<a[k]<<" ";
-                sum += a[k];
+                cout<<a[k]<<" ";
             }
-            cout<<endl<<"Sum: "<<sum<<endl;
+            cout<<endl;
         }
         cout<<endl;
     }
--- C++ program/SubarraySum.cpp
+++ C++ program/SubarraySum.cpp
@@ -32,10 +32,23 @@ int maxSubArraySum2(int a[],int n){//O(N^2)
     }
     return maxSum;
 }
+int kadaneAlgo(int a[],int n){
+    int maxSum = INT_MIN;
+    int currSum = 0;
+    for(int i=0;i<n;i++){
+        currSum += a[i];
+        maxSum = max(maxSum,currSum);
+        if(currSum<0){
+            currSum = 0;
+        }
+    }
+    return maxSum;
+}
 int main(){
-    int a[] = {12,5,-23,12,-4,11};
+    int a[] = {-5,-7,-2,2,-5,-8,-4,100};
     int n = sizeof(a)/sizeof(int);
-    int ans = maxSubArraySum2(a,n);
-    cout<<ans<<endl;
+    cout<<maxSubArraySum(a,n)<<endl;
+    cout<<maxSubArraySum2(a,n)<<endl;
+    cout<<kadaneAlgo(a,n)<<endl;
     return 0;
 }
