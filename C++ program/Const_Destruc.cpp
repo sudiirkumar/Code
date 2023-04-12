b3f9a9c3ed024a2da88f0304f609eb2e3bcfcc41
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-04-12 09:45:48 +0530

    12 April 2023

--- C++ program/Const_Destruc.cpp
+++ C++ program/Const_Destruc.cpp
@@ -1,34 +1,40 @@
 #include<iostream>
 using namespace std;
 class A{
-public:
     int a;
     int b;
     int c;
+public:
     A(int a,int b,int c){
        this->a = a;
        this->b = b;
        this->c = c;
     }
     A(){
-
+        a = b = c = 0;
     }
     A(int n){
         a = b = c = n;
     }
     ~A(){//destructor
+        this->printValues();
         cout<<"Object deleted"<<endl;
         a = 0;
         b = 0;
         c = 0;
     }
+    void printValues(){
+        cout<<a<<" "<<b<<" "<<c<<endl;
+    }
 };
 int main(){
-    A obj1(1);
-    A obj2;
-    cout<<sizeof(obj1)<<endl;
-    cout<<obj1.a<<endl;
-    cout<<obj1.b<<endl;
-    cout<<obj1.c<<endl;
+    A a1;//default
+    a1.printValues();
+    A a2(2);//parametrised
+    a2.printValues();
+    A a3(2,5,8);
+    a3.printValues();
+    A a4(a3);
+    a4.printValues();
     return 0;
 }
