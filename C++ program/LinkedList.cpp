50474c26e4ccbdfd7ff392c7e2376a3994ea40da
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-04-19 20:44:53 +0530

    19 April 2023

---
+++ C++ program/LinkedList.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(){}
+    Node(int data){
+        this->data = data;
+        next = NULL;
+    }
+};
+class LinkedList{
+    Node *head;
+    Node *tail;
+    int len;
+public:
+    LinkedList(){
+        
+    }
+    void push_back(int data){
+        
+    }
+    void push_front(int data){
+        
+    }
+    void print(){
+        
+    }
+    int length(){
+        
+    }
+    void pop_front(){
+        
+    }
+    void pop_back(){
+        
+    }
+    void reverse(){
+        
+    }
+};
+int main(){
+    LinkedList l;
+    
+    return 0;
+}
--- DSA C++/LinkedList.cpp
+++ DSA C++/LinkedList.cpp
@@ -1,22 +1,117 @@
-#include<bits/stdc++.h>
-#include"list.h"
+#include<iostream>
 using namespace std;
-int main(){
-    List l;
-    l.pushFront(1);//1
-    l.pushBack(2);//1 2
-    l.pushFront(0);//0 1 2
-    l.pushBack(5);//0 1 2 5
-    l.insert(3,3);//0 1 2 3 5
-    l.insert(4,4);//0 1 2 3 4 5
-    l.insert(-1,0);//-1 0 1 2 3 4 5
-
-    l.reverse();
-    Node* head = l.begin();
-    while(head!=NULL){
-        cout<<head->getData()<<" -> ";
-        head = head->next;
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(){}
+    Node(int data){
+        this->data = data;
+        next = NULL;
     }
-    cout<<"NULL"<<endl;
+};
+class LinkedList{
+    Node *head;
+    Node *tail;
+    int len;
+public:
+    LinkedList(){
+        head = NULL;
+        tail = NULL;
+        len = 0;
+    }
+    bool empty(){
+        if(head==NULL){
+            return true;
+        }
+        return false;
+    }
+    void push_back(int data){
+        Node *new_node = new Node(data);
+        if(head==NULL){
+            head = tail = new_node;
+        }
+        else{
+            tail->next = new_node;
+            tail = new_node;  
+        }
+        len++;
+    }
+    void push_front(int data){
+        Node *new_node = new Node(data);
+        if(head==NULL){
+            head = tail = new_node;
+        }
+        else{
+            new_node->next = head;
+            head = new_node;
+        }
+        len++;
+    }
+    void print(){
+        Node *temp = head;
+        while(temp!=NULL){
+            cout<<temp->data;
+            if(temp->next!=NULL)
+                cout<<" --> ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+    int length(){
+        return len;
+    }
+    void pop_front(){
+        if(head==NULL){
+            return;
+        }
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+        len--;
+    }
+    void pop_back(){
+        if(tail==NULL){
+            return;
+        }
+        Node *temp = head;
+        while(temp->next->next!=NULL){
+            temp = temp->next;
+        }
+        tail = temp;
+        temp = temp->next;
+        tail->next = NULL;
+        delete temp;
+        len--;
+    }
+    void reverse(){
+        Node *prev = NULL;
+        Node *curr = head;
+        Node *next;
+        while(curr!=NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+};
+int main(){
+    LinkedList l;
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+    l.push_back(5);
+    l.push_front(0);
+    l.push_front(-1);
+    l.pop_back();
+    l.pop_front();
+    l.print();
+    cout<<l.length()<<endl;
+    l.reverse();
+    l.print();
     return 0;
 }
