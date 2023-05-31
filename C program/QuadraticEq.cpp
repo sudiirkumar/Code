7acf1b6f5f125814f880869f8d5b0b13cacffa73
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-05-31 09:43:58 +0530

    31 May 2023

--- C program/QuadraticEq.cpp
+++ C program/QuadraticEq.cpp
@@ -6,18 +6,40 @@ int main(){
 	scanf("%d%d%d",&a,&b,&c);
 	float x;
 	int D = (b*b) - (4*a*c);
-	if(D<0){
-		printf("Imagenary roots");
+	float x2;
+	switch(D==0){
+		case 0:	// d is non-zero
+				switch(D>0){
+					case 0:	//d is negative
+							printf("Imagenary roots: ");
+							x = -b/(2.0*a);//real part
+							D = -D;//making D positive
+							x2 = sqrt(D)/(2*a);
+							printf("%.2f + %.2fi and %.2f - %.2fi",x,x2,x,x2);
+							break;
+					case 1:	//d is positive
+							x = (-b + sqrt(D))/(2*a);
+							printf("Unequal roots: %.2f ",x);
+							x = (-b - sqrt(D))/(2*a);
+							printf("%.2f",x);
+				}
+				break;
+		case 1:	//d is zero
+				x = -b/(2*a);
+				printf("Equal roots: %.2f %.2f",x,x);
 	}
-	else if(D==0){
-		x = -b/(2*a);
-		printf("Equal roots : %.2f %.2f",x,x);
-	}
-	else{
-		x = (-b + sqrt(D))/(2*a);
-		printf("Distinct roots: %.2f ",x);
-		x = (-b - sqrt(D))/(2*a);
-		printf("%.2f",x);
-	}
+	// if(D<0){
+	// 	printf("Imagenary roots");
+	// }
+	// else if(D==0){
+	// 	x = -b/(2*a);
+	// 	printf("Equal roots : %.2f %.2f",x,x);
+	// }
+	// else{
+	// 	x = (-b + sqrt(D))/(2*a);
+	// 	printf("Distinct roots: %.2f ",x);
+	// 	x = (-b - sqrt(D))/(2*a);
+	// 	printf("%.2f",x);
+	// }
 	return 0;
 }
