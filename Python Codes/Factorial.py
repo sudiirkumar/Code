d44fd221786726f1fc94cd3811a04958dc3b8da5
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-06-09 10:56:24 +0530

    9 June 2023

---
+++ Python Codes/Factorial.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number: "))
+fact = 1
+i = 1
+while i<=num:
+    fact *= i
+    i += 1
+print(fact)
---
+++ Python Codes/PrintDigit.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+while num>0:
+    dig = num%10
+    print(dig)
+    num = num // 10
