cf51c25585b6e0886476c8c642f883ef4cc7cf2b
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-08-12 10:17:34 +0530

    12 Aug 2023

---
+++ C program/LoopRevision.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    //Digits
+    // int n;
+    // printf("Enter a number: ");
+    // scanf("%d",&n);
+    // int sum = 0;
+    // int product = 1;
+    // while(n>0){//n!=0
+    //     int l_dig = n%10;
+    //     printf("%d ",l_dig);
+    //     sum += l_dig;
+    //     product *= l_dig;
+    //     n = n/10;
+    // }
+    // printf("\nSum of digits is %d.",sum);
+    // printf("\nProduct of digits is %d.",product);
+
+    
+    //Multiplication table
+    // 4 * 1 = 4
+    // 4 * 2 = 8
+    // int n;
+    // printf("Enter a number: ");
+    // scanf("%d",&n);
+    // for(int i=1;i<=10;i++){
+    //     printf("\n%2d * %2d = %2d",n,i,n*i);
+    // }
+    
+    //ASCII value = characters are mapped with a number -> A = 65, B = 66, ...
+    // for(int c = 0;c<128;c++){
+    //     printf("%c ",c);
+    // }
+    
+
+    //Print and sum of numbers
+    // int n;
+    // printf("Enter a number: ");
+    // scanf("%d",&n);
+    // int natural_sum = 0;
+    // printf("\nNatural numbers: \n");
+    // for(int i=1;i<=n;i++){
+    //     printf("%d ",i);
+    //     natural_sum += i;
+    // }
+    // printf("\nSum of natural number: %d",natural_sum);
+    // int odd_sum = 0;
+    // printf("\nOdd numbers: \n");
+    // for(int i=1;i<=n;i+=2){
+    //     printf("%d ",i);
+    //     odd_sum += i;
+    // }
+    // printf("\nSum of odd number: %d",odd_sum);
+    // int even_sum = 0;
+    // printf("\nEven numbers: \n");
+    // for(int i=2;i<=n;i+=2){
+    //     printf("%d ",i);
+    //     even_sum += i;
+    // }
+    // printf("\nSum of even number: %d",even_sum);
+    // int reverse_sum = 0;
+    // printf("\nNatural reverse numbers: \n");
+    // for(int i=n;i>=1;i--){
+    //     printf("%d ",i);
+    //     reverse_sum += i;
+    // }
+    // printf("\nSum of reverse number: %d",reverse_sum);
+    return 0;
+}
