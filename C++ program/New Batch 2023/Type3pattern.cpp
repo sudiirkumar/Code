b38debecbfd6b917fb2086fbd218bcd43b128d35
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-09-12 09:39:52 +0530

    12 Sept 2023

---
+++ C++ program/New Batch 2023/Type3pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 or i==n-1 or j==0 or j==n-1 or i==j or i+j==n-1 or i==(n-1)/2 or j==(n-1)/2){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
