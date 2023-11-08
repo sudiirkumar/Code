5659d04ed1775c19eb84b54520533ee0c79efd01
Sudhir Kumar <sudhir9122kumar@gmail.com>
2023-11-08 11:02:50 +0530

    8 Nov 2023

---
+++ C program/New Batch/Palindrome.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int num;//1234
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    int rev=0;//4321
+    while(num>0){
+        rev = rev*10 + (num%10);
+        num /= 10;
+    }
+    printf("Reverse: %d\n",rev);
+    if(rev==num){
+        printf("\nPalindrome");
+    }
+    else{
+        printf("\nNon Palindrome");
+    }
+    return 0;
+}
