a5c71a349037c8f38623bcbc0793bc5192b54216
Sudhir Kumar <sudhir9122kumar@gmail.com>
2024-01-05 09:05:16 +0530

    5 Jan 2024

--- C program/New Batch 2/NumberSystem.cpp
+++ C program/New Batch 2/NumberSystem.cpp
@@ -31,11 +31,51 @@ int otherToDecimal(int num,int r){
     return ans;
 }
 int main(){
-    int num,base;
-    printf("Enter a number: ");
-    scanf("%d",&num);
-    printf("Enter base to into: ");
-    scanf("%d",&base);
-    printf("Converted number: %d",decimalToOther(num,base));
+    int choice;
+    int num;
+    int temp;
+    char ch;
+    do{
+        printf("Choose any one of them: \n");
+        printf("\n1. Convert Decimal to Binary");
+        printf("\n2. Convert Decimal to Octal");
+        printf("\n3. Convert Binary to Decimal");
+        printf("\n4. Convert Binary to Octal");
+        printf("\n5. Convert Octal to Binary");
+        printf("\n6. Convert Octal to Decimal");
+        printf("\n\nEnter your choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1: printf("Enter a decimal number: ");
+                    scanf("%d",&num);
+                    printf("\nBinary number: %d",decimalToOther(num,2));
+                    break;
+            case 2: printf("Enter a decimal number: ");
+                    scanf("%d",&num);
+                    printf("\nOctal number: %d",decimalToOther(num,8));
+                    break;
+            case 3: printf("Enter a Binary number: ");
+                    scanf("%d",&num);
+                    printf("\nDecimal number: %d",otherToDecimal(num,2));
+                    break;
+            case 4: printf("Enter a Binary number: ");
+                    scanf("%d",&num);
+                    temp = otherToDecimal(num,2);
+                    printf("\nOctal number: %d",decimalToOther(temp,8));
+                    break;
+            case 5: printf("Enter a octal number: ");
+                    scanf("%d",&num);
+                    temp = otherToDecimal(num,8);
+                    printf("\nBinary number: %d",decimalToOther(temp,2));
+                    break;
+            case 6: printf("Enter a octal number: ");
+                    scanf("%d",&num);
+                    printf("\nDecimal number: %d",otherToDecimal(num,8));
+                    break;
+        }
+        printf("\nDo you want to continue?(Y/N): ");
+        fflush(stdin);
+        scanf("%c",&ch);
+    }while(ch=='Y'||ch=='y');
     return 0;
 }
